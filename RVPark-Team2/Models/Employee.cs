34148b518c3afbdadcde974dbee1ec7d38a3b4f9
benjaminfloyd1 <benjaminfloyd1@mail.weber.edu>
2026-03-24 21:17:12 -0600

    I built an employee class. I'll be working with it for adding employees to the system.

---
+++ RVPark-Team2/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Employee
+{
+	public int Id { get; set; }
+	
+	public string firstName { get; set; }
+
+	public string lastName { get; set; }
+
+	public string email { get; set; }
+
+	public string phone { get; set; }
+
+	public int age { get; set; }
+
+	public string position { get; set; }
+
+	public DateTime hireDate { get; set; }
+
+}
---
+++ RVPark-Team2/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Person
+{
+	
+	public int Id { get; set; }
+
+}
