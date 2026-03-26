93e0b66f45885677abeb093b721f7109cb8f0ceb
benjamingadd <benjamingadd@mail.weber.edu>
2026-03-25 21:13:17 -0600

    Initial rework of navigation buttons and empty admin page

---
+++ RVPark-Team2/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RVPark_Team2.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
